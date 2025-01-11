6aed4f65bfc50d142dc4d597230cdc0143c5f06f
JCGuerrero <JCystems@gMail.com>
2025-01-11 13:39:26 -0600

    feat(MessagesController)

---
+++ dotnet/WebApp/Controllers/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿namespace JCystems.SemanticKernelSampler.Dotnet.WebApp.Controllers
+{
+    using JCystems.SemanticKernelSampler.Dotnet.WebApp.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class MessagesController : ObservableControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] Request request)
+        {
+            var response = new Response
+            {
+                Request = request,
+                Message = "Times are tough",
+            };
+
+            var result = this.Ok(response);
+            return await Task.FromResult(result);
+        }
+    }
+}
---
+++ dotnet/WebApp/Models/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace JCystems.SemanticKernelSampler.Dotnet.WebApp.Models
+{
+    public class Request
+    {
+        public required Guid Id { get; set; } = Guid.NewGuid();
+
+        public required string Message { get; set; }
+    }
+}
