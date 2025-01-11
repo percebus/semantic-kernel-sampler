41f3f137f86a5152df6f9746c42cc2697543dfbf
JC Guerrero <JCystems@gMail.com>
2025-01-11 06:59:42 -0600

    refactor(semantic-kernel-py) (#5)

--- python/semantic-kernel-sampler-py/scripts/poetry/plugin/add.ba.sh
+++ python/semantic-kernel-sampler-py/scripts/poetry/plugin/add.ba.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 set -e
+
+filename="requirements.poetry-plugin.txt"
+if [[ -z $(grep '[^[:space:]]' $filename) ]]; then
+  echo "${filename} is empty, skipping..."
+  exit 0
+fi
+
 set -v
 
-cat requirements.poetry-plugin.txt | sed 's/.*/"&"/' | xargs -n 1 poetry self add
+cat ${filename} | sed 's/.*/"&"/' | xargs -n 1 poetry self add
 poetry self show plugins
 
 set +v
