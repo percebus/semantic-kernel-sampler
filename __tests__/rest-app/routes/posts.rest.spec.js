40dbc7a7039380144dfaac2ed28e677ecfccd914
JCGuerrero <JCystems@gMail.com>
2025-01-12 10:27:52 -0600

    WiP

--- __tests__/rest-app/routes/posts.rest.spec.js
+++ __tests__/rest-app/routes/posts.rest.spec.js
@@ -1,22 +1,61 @@
-describe('rest-app', () => {
-    const config = require('../config'),
-    environment = require('../../config/environments/environment'),
-    { v4: uuidv4 } = require('uuid');
+describe("rest-app", () => {
+  const config = require("../config"),
+    environment = require("../../config/environments/environment"),
+    { v4: uuidv4 } = require("uuid");
 
-    const frisby = config.frisby;
-    const schemas = {
-        Post: require('../models/Post.joi'),
-    }
+  const frisby = config.frisby;
+  const schemas = {
+    Post: require("../models/Post.joi"),
+  };
 
-    describe('posts/', () => {
-        const url = `${environment['rest-app'].url}/posts/`;
-        describe('GET', () => {
-            it("returns a list of posts", async () => {
-                return frisby
-                    .get(url)
-                    .expect("status", 200)
-                    .expect("jsonTypes", "*", schemas.Post);
-            });
-        });
+  describe("posts/", () => {
+    const url = `${environment["rest-app"].url}/posts/`;
+    describe("GET", () => {
+      it("returns a list of posts", async () => {
+        return frisby
+          .get(url)
+          .expect("status", 200)
+          .expect("jsonTypes", "*", schemas.Post);
+      });
     });
+  });
+
+  describe("/posts/-1: PUT", () => {
+    const postId = "-1";
+    const url = `${environment["rest-app"].url}/posts/${postId}`;
+    const newPost = { id: postId, title: "Some new TEST title", views: 0 };
+
+    describe("GET 1", () => {
+      it("returns the new Post", async () => {
+        return frisby.get(url).expect("status", 404);
+      });
+    });
+
+    describe("PUT", () => {
+      it("returns puts a new Post", async () => {
+        return frisby.put(url, newPost).expect("status", 200);
+      });
+    });
+
+    describe("GET 2", () => {
+      it("returns the new Post", async () => {
+        return frisby
+          .get(url)
+          .expect("status", 200)
+          .expect("jsonTypes", schemas.Post);
+      });
+    });
+
+    describe("DELETE", () => {
+      it("deletes the Post", async () => {
+        return frisby.del(url).expect("status", 200);
+      });
+    });
+
+    describe("GET 3", () => {
+      it("returns the new Post", async () => {
+        return frisby.get(url).expect("status", 404);
+      });
+    });
+  });
 });
