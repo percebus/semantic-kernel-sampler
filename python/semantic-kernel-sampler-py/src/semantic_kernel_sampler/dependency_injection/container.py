126facbd1ee4119a21984a22e847e5934f35a137
JCGuerrero <JCystems@gMail.com>
2025-08-13 17:03:51 -0500

    f

--- python/semantic-kernel-sampler-py/src/semantic_kernel_sampler/dependency_injection/container.py
+++ python/semantic-kernel-sampler-py/src/semantic_kernel_sampler/dependency_injection/container.py
@@ -3,9 +3,9 @@ from lagom.interfaces import ReadableContainer
 from semantic_kernel import Kernel
 from semantic_kernel.connectors.ai import FunctionChoiceBehavior
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion, AzureChatPromptExecutionSettings
+from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
 from semantic_kernel.contents import ChatHistory
 
-from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
 from semantic_kernel_sampler.configuration.config import Config
 from semantic_kernel_sampler.configuration.os_environ.azure_openai import AzureOpenAISettings
 from semantic_kernel_sampler.configuration.os_environ.settings import Settings
@@ -17,35 +17,46 @@ from semantic_kernel_sampler.plugins.math import MathPlugin
 from semantic_kernel_sampler.plugins.protocol import PluginProtocol
 
 
-def create_kernel(c: ReadableContainer) -> Kernel:
-    kernel = Kernel()
+def createKernel(c: ReadableContainer) -> Kernel:
+    oKernel = Kernel()
 
     plugins = c[list[PluginProtocol]]
     for plugin in plugins:
-        kernel.add_plugin(plugin, plugin_name=plugin.__class__.__name__)
+        oKernel.add_plugin(plugin, plugin_name=plugin.__class__.__name__)
 
     oAzureChatCompletion = c[AzureChatCompletion]
-    kernel.add_service(oAzureChatCompletion)
+    oKernel.add_service(oAzureChatCompletion)
 
-    return kernel
+    return oKernel
+
+
+def createChatHistory(c: ReadableContainer) -> ChatHistory:
+    oChatHistory = ChatHistory()
+
+    # TODO Read from file
+    msg = """
+        You are a helpful assistant.
+        You will only use the registered plugins.
+        If it's not in the plugins, say 'I cannot help with that.'"
+    """
+
+    oChatHistory.add_system_message(msg)
+    return oChatHistory
 
 
 container = Container()
 
-load_dotenv_files()   # TODO move to a __main__.py?
+load_dotenv_files()  # TODO move to a __main__.py?
 
 container[Config] = Singleton(Config())
 container[Settings] = lambda c: c[Config].settings
 container[AzureOpenAISettings] = lambda c: c[Settings].azure_openai
 
-container[MathPlugin] = lambda: MathPlugin()
-container[LightPlugin] = lambda: LightPlugin()
-container[list[PluginProtocol]] = lambda c: [
-    c[MathPlugin],
-    c[LightPlugin]
-]
+container[MathPlugin] = MathPlugin
+container[LightPlugin] = LightPlugin
+container[list[PluginProtocol]] = lambda c: [c[MathPlugin], c[LightPlugin]]
 
-container[ChatHistory] = ChatHistory
+container[ChatHistory] = createChatHistory
 
 container[FunctionChoiceBehavior] = Singleton(FunctionChoiceBehavior.Auto)  # type: ignore  # FIXME?
 container[PromptExecutionSettings] = lambda c: AzureChatPromptExecutionSettings(function_choice_behavior=c[FunctionChoiceBehavior])
@@ -54,6 +65,7 @@ container[AzureChatCompletion] = lambda c: AzureChatCompletion(
     base_url=c[AzureOpenAISettings].base_url,
     api_key=c[AzureOpenAISettings].api_key,
     deployment_name=c[AzureOpenAISettings].deployment_name,
-    api_version=c[AzureOpenAISettings].api_version)
+    api_version=c[AzureOpenAISettings].api_version,
+)
 
-container[Kernel] = create_kernel
+container[Kernel] = createKernel
--- python/semantic-kernel-sampler-py/src/semantic_kernel_sampler/rest/routes/messages.py
+++ python/semantic-kernel-sampler-py/src/semantic_kernel_sampler/rest/routes/messages.py
@@ -2,16 +2,17 @@ from typing import TYPE_CHECKING, Optional
 
 from flask import Flask, request
 from semantic_kernel import Kernel
-
-from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
+from semantic_kernel.connectors.ai import FunctionChoiceBehavior
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion, AzureChatPromptExecutionSettings
+from semantic_kernel.contents import ChatHistory
+
 from semantic_kernel_sampler.dependency_injection.container import container
 from semantic_kernel_sampler.rest.models.request import RequestModel
 from semantic_kernel_sampler.rest.models.response import ResponseModel
-from semantic_kernel.contents import ChatHistory
 
 if TYPE_CHECKING:
     from flask.wrappers import Response as FlaskResponse
+    from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
     from semantic_kernel.contents.chat_message_content import ChatMessageContent
 
 
@@ -31,16 +32,16 @@ async def post_async():
     oChatHistory: ChatHistory = container[ChatHistory]
     oChatHistory.add_user_message(_request.message)
 
-    oKernel:Kernel = container[Kernel]
+    oKernel: Kernel = container[Kernel]
     oPromptExecutionSettings: PromptExecutionSettings = container[AzureChatPromptExecutionSettings]
+    oPromptExecutionSettings.function_choice_behavior = FunctionChoiceBehavior.Auto()  # type: ignore # XXX FIXME
 
     oResponse: ResponseModel = ResponseModel(request=_request)
     oAzureChatCompletion: AzureChatCompletion = container[AzureChatCompletion]
 
     oChatMessageContent: Optional[ChatMessageContent] = await oAzureChatCompletion.get_chat_message_content(
-        kernel=oKernel,
-        settings=oPromptExecutionSettings,
-        chat_history=oChatHistory)
+        kernel=oKernel, settings=oPromptExecutionSettings, chat_history=oChatHistory
+    )
 
     if oChatMessageContent:
         oResponse.message = str(oChatMessageContent)
