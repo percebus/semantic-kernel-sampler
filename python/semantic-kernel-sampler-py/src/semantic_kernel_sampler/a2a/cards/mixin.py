67ad40675522c2855e4bd3981b3fc8b064219ab4
JCGuerrero <JCystems@gMail.com>
2025-08-26 17:00:39 -0500

    qf

--- python/semantic-kernel-sampler-py/src/semantic_kernel_sampler/a2a/cards/mixin.py
+++ python/semantic-kernel-sampler-py/src/semantic_kernel_sampler/a2a/cards/mixin.py
@@ -5,13 +5,10 @@ from typing import Optional
 from a2a.types import AgentCard
 
 from semantic_kernel_sampler.a2a.cards.protocol import A2ACardsProtocol
-from semantic_kernel_sampler.utils.lodash import noop
-
-noop(A2ACardsProtocol)
 
 
 @dataclass
-class A2ACardsMixin(ABC):  # , A2ACardsProtocol)
+class A2ACardsMixin(ABC, A2ACardsProtocol):
     agent_card: AgentCard = field(init=False)
 
     extended_agent_card: Optional[AgentCard] = field(default=None)
