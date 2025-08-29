bcaf59484b818d1f7c9ea74a2c7e3edb5890e4dc
JCGuerrero <JCystems@gMail.com>
2025-08-29 16:19:42 -0500

    f

--- dotnet/WebApp/Controllers/MessagesController.cs
+++ dotnet/WebApp/Controllers/MessagesController.cs
@@ -2,12 +2,24 @@
 {
     using JCystems.SemanticKernelSampler.Dotnet.WebApp.Models;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.SemanticKernel;
+    using Microsoft.SemanticKernel.ChatCompletion;
 
-    public class MessagesController : ObservableControllerBase
+    public class MessagesController(ILogger<MessagesController> logger, Kernel kernel, ChatHistory chatHistory, IChatCompletionService chatCompletionService) : ObservableControllerBase(logger)
     {
+        private Kernel Kernel { get; set; } = kernel;
+
+        private ChatHistory ChatHistory { get; set; } = chatHistory;
+
+        private IChatCompletionService ChatCompletionService => this.Kernel.GetService<IChatCompletionService>();
+
         [HttpPost]
         public async Task<IActionResult> PostAsync([FromBody] Request request)
         {
+            this.ChatHistory.AddUserMessage(request.Message);
+
+            await
+
             var response = new Response
             {
                 Request = request,
---
+++ dotnet/WebApp/ExtensionMethods/WebApplicationBuilder.cs
@@ -0,0 +1,134 @@
+﻿namespace JCystems.SemanticKernelSampler.Dotnet.WebApp.ExtensionMethods
+{
+    using System.ClientModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Text.Json;
+    using Azure;
+    using Azure.AI.OpenAI;
+    using JCystems.SemanticKernelSampler.Dotnet.WebApp.Options;
+    using Microsoft.AspNetCore.Http.Json;
+    using Microsoft.Extensions.AI;
+    using Microsoft.Extensions.Options;
+    using Microsoft.SemanticKernel;
+    using Microsoft.SemanticKernel.ChatCompletion;
+    using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+    using Scrutor;
+    using Serilog;
+
+    [ExcludeFromCodeCoverage]
+    public static class WebApplicationBuilder
+    {
+        public static Microsoft.AspNetCore.Builder.WebApplicationBuilder RegisterServices(this Microsoft.AspNetCore.Builder.WebApplicationBuilder builder)
+        {
+            builder.Configuration.AddJsonFile("appsettings.json", optional: false);
+            builder.Configuration.AddJsonFile("appsettings.ai.json", optional: false, reloadOnChange: true);
+            builder.Configuration.AddJsonFile("appsettings.ai.secrets.json", optional: false, reloadOnChange: true);
+            builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+            builder.Configuration.AddEnvironmentVariables();
+
+            builder.Services.AddOptions<AppSettingsOptions>()
+                .BindConfiguration(AppSettingsOptions.Key)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            var provider = builder.Services.BuildServiceProvider();
+
+            ILoggerFactory loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+            ILogger logger = Log.ForContext<Program>();
+
+            AppSettingsOptions appSettings;
+            try
+            {
+                // this gets us a copy of appSettings to use at startup
+                appSettings = provider.GetRequiredService<IOptionsMonitor<AppSettingsOptions>>().CurrentValue;
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "Invalid configuration, please verify that your configuration is correct. Error: {ex}", ex.Message);
+                throw;
+            }
+
+            builder.Services.AddSingleton<FunctionChoiceBehavior>(FunctionChoiceBehavior.Auto());
+            builder.Services.AddSingleton<PromptExecutionSettings>(provider =>
+            {
+                var oFunctionChoiceBehavior = provider.GetRequiredService<FunctionChoiceBehavior>();
+                return new()
+                {
+                    FunctionChoiceBehavior = oFunctionChoiceBehavior,
+                };
+            });
+
+            // SRC: https://github.com/microsoft/semantic-kernel/blob/dotnet-1.64.0/dotnet/samples/Concepts/Agents/ChatCompletion_ServiceSelection.cs
+            builder.Services.AddTransient<ApiKeyCredential>(provider => new AzureKeyCredential(appSettings.AiModel.ApiKey));
+            builder.Services.AddTransient<AzureOpenAIClient>(provider =>
+            {
+                var oApiKeyCredential = provider.GetRequiredService<ApiKeyCredential>();
+                return new(appSettings.AiModel.Endpoint, oApiKeyCredential);
+            });
+
+            builder.Services.AddTransient<ChatHistory>(provider =>
+            {
+                // TODO: Add System Message
+                return new ChatHistory();
+            });
+
+            builder.Services.AddSingleton<IChatCompletionService>(provider => new AzureOpenAIChatCompletionService(
+                endpoint: appSettings.AiModel.Endpoint.ToString(),
+                deploymentName: appSettings.AiModel.DeploymentId,
+                modelId: appSettings.AiModel.ModelId,
+                apiKey: appSettings.AiModel.ApiKey));
+
+
+            // TODO? or XXX?
+            //builder.Services.AddTransient<IChatClient>(provider =>
+            //{
+            //    var oAzureOpenAIClient = provider.GetRequiredService<AzureOpenAIClient>();
+            //    return oAzureOpenAIClient.GetChatClient(appSettings.AiModel.DeploymentId).AsIChatClient();
+            //});
+
+            builder.Services.AddTransient<IKernelBuilder>(provider => Kernel.CreateBuilder());
+
+            builder.Services.AddTransient<Kernel>(provider =>
+            {
+                var oKernelBuilder = provider.GetRequiredService<IKernelBuilder>();
+                var oAzureOpenAIClient = provider.GetRequiredService<AzureOpenAIClient>();
+                var oChatCompletionService = provider.GetRequiredService<IChatCompletionService>();
+
+                oKernelBuilder.AddAzureOpenAIChatClient(appSettings.AiModel.DeploymentId, oAzureOpenAIClient);
+
+                // oKernelBuilder.Plugins.AddFromType<YourTypeHere>(); // TODO: Add your plugins here
+
+                return oKernelBuilder.Build();
+            });
+
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddHealthChecks();
+
+            // TODO: Add resiliency
+            //builder.Services.ConfigureHttpClientDefaults(http =>
+            //{
+            //    // Turn on resilience by default
+            //    http.AddStandardResilienceHandler();
+            //});
+
+            builder.Services.Configure<JsonOptions>(oJsonOptions =>
+            {
+                JsonSerializerOptions oJsonSerializerOptions = oJsonOptions.SerializerOptions;
+                oJsonSerializerOptions.PropertyNameCaseInsensitive = false;
+                oJsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            });
+
+            builder.Services.Scan(
+                s => s.FromAssemblyOf<Program>()
+                      .AddClasses()
+                      .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                      .AsMatchingInterface());
+
+            return builder;
+        }
+    }
+}
