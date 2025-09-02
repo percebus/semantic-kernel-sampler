f151187ed3df444333c1ee6fef8b85f0437c2da1
JCGuerrero <JCystems@gMail.com>
2025-09-02 15:14:51 -0500

    e

--- dotnet/WebApp/Services/IBuiltInAgent.cs
+++ dotnet/WebApp/Services/IBuiltInAgent.cs
@@ -4,6 +4,13 @@
 
     public interface IBuiltInAgent
     {
+        // TODO REFACTOR: See below
         public Task<IReadOnlyList<KernelContent>> InvokeAsync(IReadOnlyList<KernelContent> messages);
+
+        // public abstract IAsyncEnumerable<AgentResponseItem<ChatMessageContent>> InvokeAsync(
+        //     ICollection<ChatMessageContent> messages,
+        //     AgentThread? thread = null,
+        //     AgentInvokeOptions? options = null,
+        //     CancellationToken cancellationToken = default);
     }
 }
