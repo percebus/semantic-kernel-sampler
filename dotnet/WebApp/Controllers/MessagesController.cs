4f881c1f2f9c1476a9c4b13c091c768b7f78f46f
JCGuerrero <JCystems@gMail.com>
2025-09-02 17:08:22 -0500

    ff

--- dotnet/WebApp/Controllers/MessagesController.cs
+++ dotnet/WebApp/Controllers/MessagesController.cs
@@ -1,33 +1,41 @@
 ﻿namespace JCystems.SemanticKernelSampler.Dotnet.WebApp.Controllers
 {
     using JCystems.SemanticKernelSampler.Dotnet.WebApp.Models;
-    using JCystems.SemanticKernelSampler.Dotnet.WebApp.Services;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.SemanticKernel;
+    using Microsoft.SemanticKernel.Agents;
     using Microsoft.SemanticKernel.ChatCompletion;
 
-    public class MessagesController(ILogger<MessagesController> logger, ICustomAgent agent) : ObservableControllerBase(logger)
+    public class MessagesController(ILogger<MessagesController> logger, Agent agent) : ObservableControllerBase(logger)
     {
-        private IBuiltInAgent Agent => agent;
+        private Agent Agent => agent;
 
         [HttpPost]
         public async Task<IActionResult> PostAsync([FromBody] Request request)
         {
             this.Logger.LogInformation("Received request: {Request}", request);
             var requestChatMessageContent = new ChatMessageContent(AuthorRole.User, request.Message);
-            var requestMessages = new ChatMessageContentItemCollection { requestChatMessageContent };
-            IReadOnlyList<KernelContent> replyMessages = await this.Agent.InvokeAsync(requestMessages);
-            KernelContent firstReplyChatMessageContent = replyMessages[0];
+            var requestMessages = new List<ChatMessageContent> { requestChatMessageContent };
 
-            var response = new Response
+            AgentThread? thread = null;
+            await foreach (var item in this.Agent.InvokeAsync(requestMessages, thread))
             {
-                Request = request,
-                Message = firstReplyChatMessageContent.ToString(),
-            };
+                thread = item.Thread;
+                this.Logger.LogInformation("Request message content item: {Item}", item);
 
-            this.Logger.LogInformation("Sending response: {Response}", response);
-            var result = this.Ok(response);
-            return await Task.FromResult(result);
+                ChatMessageContent responseChatMessageContent = item.Message;
+                var response = new Response
+                {
+                    Request = request,
+                    Message = responseChatMessageContent.Content,
+                };
+
+                this.Logger.LogInformation("Sending response: {Response}", response);
+                var result = this.Ok(response);
+                return await Task.FromResult(result);
+            }
+
+            throw new Exception("No response received");
         }
     }
 }
--- dotnet/WebApp/Services/CustomChatAgent.cs
+++ dotnet/WebApp/Services/CustomChatAgent.cs
@@ -1,6 +1,10 @@
 ﻿namespace JCystems.SemanticKernelSampler.Dotnet.WebApp.Services
 {
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Microsoft.SemanticKernel;
+    using Microsoft.SemanticKernel.Agents;
     using Microsoft.SemanticKernel.ChatCompletion;
 
     public class CustomChatAgent(ILogger<ICustomChatAgent> logger, IChatCompletionService chatCompletionService, ChatHistory chatHistory) : ICustomChatAgent
@@ -13,12 +17,17 @@
         // TODO? public?
         private IChatCompletionService ChatCompletionService => chatCompletionService;
 
-        public async Task<KernelContent> InvokeAsync(KernelContent userMessage)
+        public async Task<ChatMessageContent> InvokeAsync(ChatMessageContent userMessage)
         {
             this.Logger.LogInformation("Invoking CustomChatAgent with user message: {userMessage}", userMessage);
 
-            string userMessageString = userMessage.ToString() ?? string.Empty;
-            this.ChatHistory.AddUserMessage(userMessageString);
+            string? userMessageContent = userMessage.Content;
+            if (string.IsNullOrEmpty(userMessageContent))
+            {
+                throw new ArgumentException("User message content cannot be null or empty.", nameof(userMessage));
+            }
+
+            this.ChatHistory.AddUserMessage(userMessageContent);
 
             // SRC: https://github.com/microsoft/semantic-kernel/blob/dotnet-1.64.0/dotnet/samples/Concepts/ChatCompletion/AzureOpenAI_ChatCompletion.cs
             ChatMessageContent replyChatMessageContent = await this.ChatCompletionService.GetChatMessageContentAsync(this.ChatHistory);
@@ -30,12 +39,22 @@
             return replyChatMessageContent;
         }
 
-        public async Task<IReadOnlyList<KernelContent>> InvokeAsync(IReadOnlyList<KernelContent> requestMessages)
+
+        public async IAsyncEnumerable<AgentResponseItem<ChatMessageContent>> InvokeAsync(
+            ICollection<ChatMessageContent> messages,
+            AgentThread? thread = null,
+            AgentInvokeOptions? options = null,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
-            ChatMessageContent firstRequestMessage = (ChatMessageContent)requestMessages[0];
-            KernelContent replyMessage = await this.InvokeAsync(firstRequestMessage);
-            var replyMessages = new ChatMessageContentItemCollection { replyMessage };
-            return replyMessages;
+            ChatMessageContent firstRequestMessage = messages.First();
+            ChatMessageContent replyMessage = await this.InvokeAsync(firstRequestMessage);
+
+            if (thread is null)
+            {
+                throw new ArgumentNullException(nameof(thread), "AgentThread cannot be null.");
+            }
+
+            yield return new AgentResponseItem<ChatMessageContent>(replyMessage, thread);
         }
     }
 }
