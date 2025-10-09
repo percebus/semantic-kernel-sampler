a380bad50c5519dd819119bf2b7fc9bca2d750c9
JCGuerrero <JCystems@gMail.com>
2025-10-09 11:35:07 -0500

    feat: agent-framework-py

---
+++ python/agent-framework-py/scripts/setup.ba.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+target_config=$1
+echo "target_config:'${target_config}'"
+
+scripts_path="$(dirname "$(readlink -f "$0")")"
+echo "Script directory: ${scripts_path}"
+
+set -x
+
+# Upgrades top-level dependencies, like pipx
+bash ${scripts_path}/pip/upgrade.ba.sh
+
+# pipx installs CLI executables, like poetry
+bash ${scripts_path}/pipx/install.ba.sh
+
+# uv tool install
+bash ${scripts_path}/uv/tool/install.ba.sh ${target_config}
+
+# uv sync
+uv run -- poe sync
+
+# uv build
+uv run -- poe build
+
+set +x
+set +e
