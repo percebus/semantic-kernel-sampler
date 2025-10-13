b0275ffa12bc46cd58fd60e1c8925a61b3355427
JCGuerrero <JCystems@gMail.com>
2025-10-13 10:54:40 -0500

    wip

---
+++ dotnet/AgentFrameworkSampler.DotNet/WebApp/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi(); // UseSwaggerUI Protected by if (env.IsDevelopment())
+}
+
+// TODO Move to appSettings.json
+string? isDotNETRunningInContainer = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER");
+
+// Only use HTTPS redirection when not running in a container
+if (string.IsNullOrEmpty(isDotNETRunningInContainer))
+{
+    app.UseHttpsRedirection();
+}
+else
+{
+    // Skip HTTPS redirection in containers
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
