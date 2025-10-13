8add0f4ab37eb7d9964d275e741c0c3503de38ec
JCGuerrero <JCystems@gMail.com>
2025-10-13 14:28:02 -0500

    f

---
+++ dotnet/AgentFrameworkSampler.DotNet/Shared/Options/AppSettingsOptions.cs
@@ -0,0 +1,25 @@
+﻿namespace JCystems.AgentFrameworkSampler.Dotnet.Shared.Options
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.Extensions.Options;
+    
+    public class AppSettingsOptions
+    {
+        public const string Key = "JCystems:AgentFrameworkSampler.DotNet.WebApp";
+
+        [ValidateObjectMembers]
+        [Required(ErrorMessage = ErrorMessages.AiModelOptionsRequired)]
+        public AiModelOptions AiModel { get; set; } = new();
+
+        [ValidateObjectMembers]
+        [Required(ErrorMessage = ErrorMessages.AzureCredentialOptionsRequired)]
+        public AzureCredentialOptions AzureCredential { get; set; } = new();
+
+        public static class ErrorMessages
+        {
+            public const string AiModelOptionsRequired = $"{Key}:{nameof(AiModel)} cannot be empty";
+
+            public const string AzureCredentialOptionsRequired = $"{Key}:{nameof(AzureCredential)} cannot be empty";
+        }
+    }
+}
