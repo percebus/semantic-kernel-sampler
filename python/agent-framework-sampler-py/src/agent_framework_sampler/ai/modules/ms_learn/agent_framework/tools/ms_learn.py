2c5aa205725b1044fd500e1760f6da03e64ddc1c
JCGuerrero <JCystems@gMail.com>
2025-12-02 16:03:12 -0600

    feat(`agent-framework-sampler-py`): `ms-learn-agent` (MCP)

---
+++ python/agent-framework-sampler-py/src/agent_framework_sampler/ai/modules/ms_learn/agent_framework/tools/ms_learn.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+
+from agent_framework import MCPStreamableHTTPTool
+
+from agent_framework_sampler.config.configuration import Configuration
+
+
+@dataclass
+class MSLearnMCPStreamableHttpTool(MCPStreamableHTTPTool):
+    configuration: Configuration = field()
+
+    description: str = field(default="MCP tool description")
+
+    url: str = field(default="https://learn.microsoft.com/api/mcp")
+
+    def __post_init__(self) -> None:
+        return super().__init__(
+            name=self.__class__.__name__,
+            description=self.description,
+            url=self.url,
+        )
